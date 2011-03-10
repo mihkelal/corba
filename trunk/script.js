eac85d75e4922db48bb4499f8a07241462425aac
mihkel_alavere <mihkel_alavere@hotmail.com>
2011-03-10 15:50:51 +0000

    Check nupule JS, JS eraldi failis

---
+++ trunk/script.js
@@ -0,0 +1,10 @@
+function show_alert() {
+	alert("You bet xxx money");
+//	var betamount = document.getElementById('Bet').value;
+//	alert("You bet " + betamount + " money");
+}
+
+function show_prompt() {
+	var checkamount = prompt("Please enter your name", "Harry Potter");
+	alert("You checked " + checkamount + " money");
+}
